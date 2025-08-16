ea12ab0b64d212e45f0d1f37d99d6ca88c8ed633
dilruba-sarker <dilrubasarkerseo@gmail.com>
2025-08-16 21:43:41 +0600

    bg add

--- src/Component/BannerWithSearch/Banner.jsx
+++ src/Component/BannerWithSearch/Banner.jsx
@@ -1,10 +1,17 @@
 
 import React from "react";
-import bg from '../../assets/bg.png'
+import bg from '../../assets/e.png'
 const Banner = ({ searchInput, setSearchInput, handleSearch }) => {
   return (
-    <div className="bg-blue-100 py-10 px-4 mb-10  h-64" style={{backgroundImage:`url(${bg})`}}>
-      <h1 className="text-3xl font-bold text-center text-amber-50 mb-4">Discover Posts by Tag</h1>
+    <div className="bg-blue-100 py-10 px-4 mb-10  h-96"
+      style={{
+         backgroundImage: `linear-gradient(to bottom, rgba(0,0,0,0.6), rgba(0,0,0,0.4)), url(${bg})`,
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
+     
+     >
+      <h1 className="text-3xl font-bold items-center text-center text-amber-50 mb-4">Discover Posts by Tag</h1>
       <form
         onSubmit={handleSearch}
         className="flex justify-center gap-2 max-w-md mx-auto"
